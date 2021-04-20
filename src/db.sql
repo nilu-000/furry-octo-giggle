ca0844efc64fb96f3682c91fc3a76d1eb67f829e
nilu-000 <nilufartokhirova000@gmail.com>
2021-04-20 11:43:59 +0500

    edit db

--- src/db.sql
+++ src/db.sql
@@ -1,18 +1,20 @@
 CREATE DATABASE bookstore;
-CREATE TABLE books({
-    id VARCHAR(32) NOT NULL AUTO_INCREMENT,
+CREATE TABLE books(
+    id SERIAL NOT NULL,
     name VARCHAR(32) NOT NULL,
     PRIMARY KEY(id)
-});
+);
 
-CREATE TABLE authors({
-    id VARCHAR(32) NOT NULL AUTO_INCREMENT,
+CREATE TABLE authors(
+    id SERIAL NOT NULL,
     fullname VARCHAR(32) NOT NULL,
     email VARCHAR(32) NOT NULL UNIQUE,
     PRIMARY KEY(id)
-});
+);
 
-CREATE TABLE book_author({
-    book_id int FOREIGN KEY REFERENCES book(id),
-    author_id int FOREIGN KEY REFERENCES author(id)
-});
+CREATE TABLE book_author(
+    CONSTRAINT book_id 
+        FOREIGN KEY(id) REFERENCES books(id),
+    CONSTRAINT author_id 
+        FOREIGN KEY(id) REFERENCES authors(id)
+);
